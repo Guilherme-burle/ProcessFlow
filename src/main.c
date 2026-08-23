25f8f198f76960b07f429c5a0a48cbd13e3865af
Guilherme-burle <guiburle1320@gmail.com>
2026-08-23 00:38:15 -0300

    tokenização e struct para 1 tasks

--- src/main.c
+++ src/main.c
@@ -2,14 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct{
+    char nome[64];
+    char programa[128];
+    char *argumentos[32];
+} Task;
+
+
 int main(){
-    char comando[128];
+    char linha[256];
+    Task t;
     while(1){
         printf("processflow> ");
-        fgets(comando, sizeof(comando), stdin);
-        comando[strcspn(comando, "\n")] = '\0';
-        if (strcmp(comando, "exit") == 0){
+        if (fgets(linha, sizeof(linha), stdin) == NULL) {
             break;
+        }
+        char *token = strtok(linha, " \t\n");
+         if (token == NULL){
+            printf("Nenhum comando digitado.\n");
+            continue;
+        }
+        if (strcmp(token, "exit") == 0){
+            break;
+        }
+
+        if (strcmp(token, "task") == 0){
+            token = strtok(NULL, " \t\n");
+            if (token != NULL) {
+            strncpy(t.nome, token, 64);
+            }
+
+            token = strtok(NULL, " \t\n");
+            if (token != NULL) {
+            strncpy(t.programa, token, 128);
+            }
+
+            token = strtok(NULL, " \t\n");
+            int i = 0;
+            while(token != NULL && i<32) {
+            t.argumentos[i] = token;
+            i++;
+            token = strtok(NULL, " \t\n");
+            }
+            t.argumentos[i] = NULL;
+
+            printf("\n--- Tarefa Processada ---\n");
+            printf("Nome da tarefa : %s\n", t.nome);
+            printf("Programa       : %s\n", t.programa);
+            printf("Argumentos     : ");
+            for (int i = 0; t.argumentos[i] != NULL; i++) {
+                printf("[%s] ", t.argumentos[i]);
+            }
+            printf("\n");
+        } else {
+        printf("Comando desconhecido: %s\n", token);
         }
     }
     return 0;
